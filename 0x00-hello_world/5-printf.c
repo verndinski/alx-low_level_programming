e1a872c19b32b6c1cdcca4785c49fbe144a15c56
verndinski <zainazayuni@gmail.com>
2023-10-05 10:33:54 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * Main - Entry point
- *
- * Return: Always 0 (Success)
+ * Main - A program that print a line using the printf
+ unction
+ * Return:0 (Success)
  */
+
 int main(void)
 {
-	printf("With proper grammer, but the out put is a pieces of art,\n");
+	printf("With proper grammer, but the outcome is a pieces of art,\n")
 	return (0);
 }
-
