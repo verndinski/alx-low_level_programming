6adabba3b67aa613e8d2e00b910b436e80108004
verndinski <zainazayuni@gmail.com>
2023-10-20 10:09:58 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,17 +2,17 @@
 
 void print_buffer(char *b, int size)
 {
-	if (size <= 0) 
+	if(size <= 0) 
 	{
 	putchar('\n');
 	return;
 	}
-	for (int i = 0; i < size; i += 10) 
+	for(int i = 0; i < size; i += 10) 
 	{
 	printf("%08x ", i);
 
 	// Print the hexadecimal content
-	for (int j = i; j < i + 10; j++) 
+	for(int j = i; j < i + 10; j++) 
 	{
 	if (j < size)
 		printf("%02x ", (unsigned char)b[j]);
@@ -21,15 +21,15 @@ void print_buffer(char *b, int size)
 	}
 	printf(" ");
 	// Print the ASCII content
-	for (int j = i; j < i + 10; j++) 
+	for(int j = i; j < i + 10; j++) 
 	{
-	 if (j < size) 
-	{
-		if (b[j] >= 32 && b[j] <= 126) 
+	 if(j < size); 
+	}		
+		if(b[j] >= 32 && b[j] <= 126) 
 	{
 		putchar(b[j]);
 	}
-	else 
+	else
 	{
 		putchar('.');
 	}
@@ -39,9 +39,9 @@ void print_buffer(char *b, int size)
 	}
 }
 
-int main(void)
+int main(void;)
 {
-	char buffer[30] = "This is a test buffer!";
+	char buffer[30] = ("This is a test buffer!");
 	int size = 20;
 	print_buffer(buffer, size);
 	return 0;
