eebf3023e211569cece4a9f3a1199b930ef7882d
verndinski <zainazayuni@gmail.com>
2023-10-20 10:01:44 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,44 +2,48 @@
 
 void print_buffer(char *b, int size)
 {
-    if (size <= 0) {
-        putchar('\n');
-        return;
-    }
+	if (size <= 0) 
+	{
+	putchar('\n');
+	return;
+	}
+	for (int i = 0; i < size; i += 10) 
+	{
+	printf("%08x ", i);
 
-    for (int i = 0; i < size; i += 10) {
-        printf("%08x ", i);
-
-        // Print the hexadecimal content
-        for (int j = i; j < i + 10; j++) {
-            if (j < size)
-                printf("%02x ", (unsigned char)b[j]);
-            else
-                printf("   "); // Print spaces for padding
-        }
-
-        printf(" ");
-
-        // Print the ASCII content
-        for (int j = i; j < i + 10; j++) {
-            if (j < size) {
-                if (b[j] >= 32 && b[j] <= 126) {
-                    putchar(b[j]);
-                } else {
-                    putchar('.');
-                }
-            }
-        }
-
-        putchar('\n');
-    }
+	// Print the hexadecimal content
+	for (int j = i; j < i + 10; j++) 
+	{
+	if (j < size)
+		printf("%02x ", (unsigned char)b[j]);
+	else
+		printf("   "); // Print spaces for padding
+	}
+	printf(" ");
+	// Print the ASCII content
+	for (int j = i; j < i + 10; j++) 
+	{
+	 if (j < size) 
+	{
+		if (b[j] >= 32 && b[j] <= 126) 
+	{
+		putchar(b[j]);
+	}
+	else 
+	{
+		putchar('.');
+	}
+	}
+	}
+		putchar('\n');
+	}
 }
 
 int main(void)
 {
-    char buffer[30] = "This is a test buffer!";
-    int size = 20;
-    print_buffer(buffer, size);
-    return 0;
+	char buffer[30] = "This is a test buffer!";
+	int size = 20;
+	print_buffer(buffer, size);
+	return 0;
 }
 	
