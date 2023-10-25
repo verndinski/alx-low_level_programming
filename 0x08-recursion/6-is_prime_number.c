2923685d9eb816256326497a6873a4b27b75c510
verndinski <zainazayuni@gmail.com>
2023-10-25 09:56:24 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -17,3 +17,20 @@ int is_prime_number(int n)
 		return (0);
 	return (is_prime(n, i));
 }
+
+/**
+ * is_prime - returns 1 if the input integer is a prime number,
+ * otherwise returns 0
+ *
+ * @n: number
+ * @i: dividend
+ * Return: int
+ */
+int is_prime(int n, int i)
+{
+	if (n != i && n % i == 0)
+		return (0);
+	else if (n == i)
+		return (1);
+	return (is_prime(n, i + 1));
+}
