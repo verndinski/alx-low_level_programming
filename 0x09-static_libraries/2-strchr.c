b5e0c1d014118240a66025d2559d86e218c991f9
verndinski <zainazayuni@gmail.com>
2023-10-28 08:44:47 -0700

    2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,10 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
+/**
+ * _strchr - Program entry point
+ * @s: input value
+ * @c: input value
+ * Return: Always 0 (success)
+ */
 char *_strchr(char *s, char c)
 {
-    return strchr(s, c);
+	int a = 0;
+
+	for (; s[a] >= '\0'; a++)
+	{
+		if (s[a] ==c)
+			return (&s[a]);
+	}
+	return (0);
 }
