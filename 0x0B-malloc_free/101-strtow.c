160b6f6797c93f0dfc7138365697ab1afdf15d43
verndinski <zainazayuni@gmail.com>
2023-10-30 10:54:56 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,50 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-/**
-  *argstostr - concatenates all arguments of the program.
-  *@ac: argument count.
-  *@av: pointer to array of size ac.
-  *Return: NULL if ac == 0 or av == null, Pointer to new string.
-  *NULL on fail.
-  */
-char *argstostr(int ac, char **av)
-{
-	int i, j, k, size;
-	char *arg;
 
-	size = 0;
-	k = 0;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	i = 0;
-	while (i < ac)
+/** 
+ *  print_tab - prints an array of string
+ *  @tab: The array to print
+ *
+ *  Return: nothing
+ */
+void print_tab(char **tab)
+{
+	int i;
+
+	for (i = 0;mtab[i] != NULL; ++i)
 	{
-		j = 0;
-		while (av[i][j])
-		{
-			size++;
-			j++;
-		}
-		size++;
-		i++;
+		printf("%s\n", tab[i]);
 	}
-	arg = malloc((sizeof(char) * size) + 1);
-	if (arg == NULL)
-		return (NULL);
-	i = 0;
-	while (i < ac)
-	{
-		j = 0;
-		while (av[i][j])
-		{
-			arg[k] = av[i][j];
-			j++;
-			k++;
-		}
-		arg[k] = '\n';
-		k++;
-		i++;
-	}
-	arg[k] = '\0';
-	return (arg);
 }
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: 1 if an error occurred, 0 otherwise
+ */
+int main(void)
+{
+	char **tab;
+
+	tab = strtow("		ALX School		#cisfun		");
+	if (tab == NULL)
+	{
+	printf("failled\n");
+	return (1);
+	}
+	print_tab(tab);
+	return (0);
+}	
