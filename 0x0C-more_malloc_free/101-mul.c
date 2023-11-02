20086c02faaa22d584e3e9595c18095b64bffc46
verndinski <zainazayuni@gmail.com>
2023-11-02 11:31:22 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -50,23 +50,23 @@ void errors(void)
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
- * @argc: array of arguments
+ * @argv: array of arguments
  *
  * Return: always 0 (success)
  */
 int main(int argc, char *argc[])
 {
 	char *s1, *s2;
-	int len1, lens2, len, i, carry, digit1, digit2, *results, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *results, a = 0;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digits(s1) || !is_digit(s2))
+	s1 = argc[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
 	results = malloc(sizeof(int) * len);
-	if (!result)
+	if (!results)
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		results[i] = 0;
@@ -88,7 +88,7 @@ for (i = 0; i < len - 1; i++)
 {
 	if (result[i])
 		a = 1;
-	if (a)
+	if (!a)
 		_putchar(result[i] + '0');
 }
 if (!a)
