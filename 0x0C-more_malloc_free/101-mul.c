72651295d631b9a36a53bf11c236f61a46d0192d
verndinski <zainazayuni@gmail.com>
2023-11-02 12:40:13 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,24 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int is_positive_integer(char *str)
-{
-	while (*str)
-	{
-	if (!isdigit(*str))
-	{
-	return (0);
-	}
-	str++;
-	}
-	return (1);
-}
-
-int multiply_numbers(int num1, int num2)
-{
-	return (num1 * num2);
-}
-
 int main(int argc, char *argv[])
 {
 	char *num1_str, *num2_str;
@@ -34,7 +16,6 @@ int main(int argc, char *argv[])
 
 	num1_str = argv[1];
 	num2_str = argv[2];
-
 	if (!is_positive_integer(num1_str) || !is_positive_integer(num2_str))	
 	{
 	printf("Error\n");
