0d709fbe03dbd0a8b9d29bd9fbb92263652376ce
verndinski <zainazayuni@gmail.com>
2023-11-02 07:59:26 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_digit(char c) {
+    return (c >= '0' && c <= '9');
+}
+
+int main(int argc, char *argv[]) {
+    char *num1_str;
+    char *num2_str;
+    long num1, num2, result;
+    int i;
+
+    if (argc != 3) {
+        printf("Error\n");
+        return 98;
+    }
+
+    num1_str = argv[1];
+    num2_str = argv[2];
+
+    for (i = 0; num1_str[i] != '\0'; i++) {
+        if (!is_digit(num1_str[i])) {
+            printf("Error\n");
+            return 98;
+        }
+    }
+
+    for (i = 0; num2_str[i] != '\0'; i++) {
+        if (!is_digit(num2_str[i])) {
+            printf("Error\n");
+            return 98;
+        }
+    }
+
+    num1 = atol(num1_str);
+    num2 = atol(num2_str);
+
+    result = num1 * num2;
+
+    printf("%ld\n", result);
+
+    return 0;
+}
