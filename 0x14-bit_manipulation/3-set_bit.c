a043f2f9b9f9a864cbf8ccc257bdfeb5daacd2db
verndinski <zainazayuni@gmail.com>
2023-11-23 11:49:11 -0800

    0-binary_to_uint.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,7 +8,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int mask;
 
-	if  (index > sizeof(unsigned int) *8)
+	if  (index > sizeof(unsigned int) * 8)
 		return (-1);
 	mask = 1;
 	mask = mask << index;
